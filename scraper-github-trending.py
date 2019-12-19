aa4eaee09abb5cb53c40894c47bdc94c772f2f75
dpresident <daniel.ad0791@gmail.com>
2019-12-19 13:26:42 +0100

    Update scraper-github-trending.py

--- scraper-github-trending.py
+++ scraper-github-trending.py
@@ -1,34 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import csv
+
 #pasos siguientes a desarrollar
 #0. Agregar variables adicionales
-#1. Web scraping automatizado
-#2. Archivo CSV con variables
+#1. Archivo CSV con variables
+#2. Web scraping automatizado
+
 
 page = requests.get('https://www.alibaba.com/product-detail/Portable-Small-USB-Travel-LED-Makeup_60830030133.html?spm=a2700.details.maylikever.2.1fb53cc2uSVPvx')
 
 # Create a BeautifulSoup object
 soup = BeautifulSoup(page.text, 'html.parser')
-#print(soup.get_text())
+
 
 #v1 = soup.head.title
 #print(v1)
 
 v2 = soup.select_one('meta[property="og:price:amount"][content]')['content']
-print("v2 is",v2)
+print("Min Price:",v2)
 
 v3 = soup.select_one('meta[property="og:price:standard_amount"][content]')['content']
-print("v3 is",v3)
+print("Std Price:",v3)
 
 v4 = soup.title.string
 print(v4)
 
 ###
 company_name = soup.select_one('[data-aui="company-name"][title]')['title']
-print("v5 is", company_name)
+print("Company Name:", company_name)
 
-
+### Company Product Id & Company Id
 raw_content = soup.select_one('meta[name="aplus-exdata"][content]')['content']
 #parse raw json
 product_and_company_meta = json.loads(raw_content)
@@ -41,8 +44,19 @@ v6 = soup.select_one('meta[name="aplus-exdata"][content]')['content']
 print("v6 is",v6)    
     
 ###
+#v7 = soup.select_one('a[class="score-lite"][content]')['content']
+#print("v7 is",v7) 
 
-#v5 = soup.a.find(['class="company-name company-name-lite-vb"'])["title"]
+###CSV
+#Open file to write
+with open('test1.csv', 'w', newline='') as f:
+    thewriter = csv.writer(f)
+# write headers
+    thewriter.writerow(['Company Name', 'Company Id', 'Product Id', 'Standard Price'])
+#Write rows
+    thewriter.writerow([company_name, product_and_company_meta['productId'], product_and_company_meta['companyId'], v3 + '$'])
+
+
 
 #Alibaba
 #<meta property="og:price:amount" content="1.89"/>
@@ -55,3 +69,5 @@ print("v6 is",v6)
 
 #Product and Company id
 # <meta name="aplus-exdata" content='{"productId":"60769168637","companyId":"240628921"}'>
+
+# Xpath example: /html/body/table/tbody/tr[123]
