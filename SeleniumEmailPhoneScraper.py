834df647ac015b6ebf74acb484eb375b72c8d869
dpresident <daniel.ad0791@gmail.com>
2019-12-23 12:25:40 +0100

    Update and rename SeleniumEmailScrapper.py to SeleniumEmailPhoneScraper.py

--- SeleniumEmailPhoneScraper.py
+++ SeleniumEmailPhoneScraper.py
@@ -14,6 +14,10 @@ browser.maximize_window()
 browser.get('https://www.aeropuertomadrid-barajas.com/eng/madrid-barajas-airport-info.htm')
 doc = browser.page_source #page source, we are going to look into it
 emails = re.findall(r'[\w\.-]+@[\w\.-]+', doc) #search for emails using regular expresions
+Phones = re.findall(r'[(][\D]{1}[\d]{2}[)][\D]{1}[\d]{3}[\D]{1}[\d]{3}[\D]{1}[\d]{3}', doc) #phone numbers, format: (+34) 91 321 10 00.
 
-for email in emails:
+for email in emails: #for an element inside the document 'doc', search for sub elements
     print(email)
+
+for phone in Phones:
+    print(phone)
