6f66883c8a55e11ad2b0ad3f5b06741c32c634f5
dpresident <daniel.ad0791@gmail.com>
2020-01-07 21:08:31 +0100

    Update Selenium Login.py

--- Selenium Login.py
+++ Selenium Login.py
@@ -4,18 +4,17 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
 from selenium.webdriver import Chrome
+import time
 import re #regular expressions, are imported from python directly
 
 driver = webdriver.Firefox()
 driver.get('https://www.ssactivewear.com/')
-driver.find_elements_by_class_name('ltkpopup-close')
-#driver.find_elements_by_class_name('btn2').click()
-#driver.find_elements_by_class_name("dxic").send_keys('predictstore@predictvia.com')
-
-#driver.find_element_by_name('LOGIN').click()
-
-#def ss_login():
- #   driver.get('https://www.ssactivewear.com/')
-  #  driver.find_element_by_name('LOGIN').click()
-    #driver.find_element_by_id('').send_keys('')
-    #driver.find_element_by_id('').click()
+time.sleep(5)
+driver.find_element_by_id("ltkpopup-close-button").click()
+time.sleep(2)
+driver.find_element_by_class_name('btn2').click()
+time.sleep(3)
+driver.find_element_by_id("M_M_zEmailTB").send_keys('predictstore@predictvia.com')
+time.sleep(2)
+driver.find_element_by_id("M_M_zPasswordTB_CLND").send_keys('Isapass321')
+driver.find_element_by_id('M_M_zPageLoginBTN_CD').click()
